b849c93ce9afa2ba6b17f907f573b5ed7a6768ec
Gourav Kumar Sharma <gourav0002006@gmail.com>
2026-08-16 21:46:31 +0530

    Time: 41 ms (5.03%), Space: 46.5 MB (76.84%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,29 +1,45 @@
 class Solution {
     public String convert(String s, int numRows) {
+
         if (numRows == 1 || numRows >= s.length()) {
             return s;
         }
+
         int[] arr = new int[s.length()];
-        int n = numRows;
+
         int k = 0;
-        arr[0] = 0;
         int direction = 1;
-        for(int i = 1;i<arr.length;i++){
-            k = k+direction;
+
+        arr[0] = 0;
+
+        // Store the row number of each character
+        for (int i = 1; i < s.length(); i++) {
+
+            k = k + direction;
             arr[i] = k;
-            if(k==numRows-1)
+
+            if (k == numRows - 1) {
                 direction = -1;
-            if(k==0)
-                direction =1;
+            }
+
+            if (k == 0) {
+                direction = 1;
+            }
         }
+
         StringBuilder ans = new StringBuilder();
-        for(int j=0;j<=numRows-1;j++){
-            for(int i =0;i<s.length();i++){
-                if(arr[i]==j){
+
+        // Read row by row
+        for (int j = 0; j < numRows; j++) {
+
+            for (int i = 0; i < s.length(); i++) {
+
+                if (arr[i] == j) {
                     ans.append(s.charAt(i));
                 }
             }
         }
+
         return ans.toString();
     }
 }
