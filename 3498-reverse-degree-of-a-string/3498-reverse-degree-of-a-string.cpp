2732c5aa253cfd6611fbdb1bc229a9389707006e
Gourav Kumar Sharma <gourav0002006@gmail.com>
2026-09-20 18:03:35 +0530

    Time: 0 ms (100%), Space: 9.8 MB (47.88%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -3,10 +3,7 @@ public:
     int reverseDegree(string s) {
         int ans =0;
         for(int i = 0;i<s.length();i++){
-            int val = 'z'-s[i]+1;
-            int idx = i+1;
-            int prod = val*idx;
-            ans += prod;
+            ans += ('z'-s[i]+1)*(i+1);
         }
         return ans;
     }
